6b7c3389450d7d8d85c1384e209ec4cb7d883d50
Danil <danilbarabash@icloud.com>
2025-08-18 15:34:53 +0300

    Redisign activity_statistics Fix button activity_learn_word

--- app/src/main/java/com/example/englishwordsappn/StatisticsActivity.kt
+++ app/src/main/java/com/example/englishwordsappn/StatisticsActivity.kt
@@ -11,10 +11,7 @@ class StatisticsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_statistics)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
     }
 }
