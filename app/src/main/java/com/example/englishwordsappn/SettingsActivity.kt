6480dec71ea840397641e65eaf6d0daecfd83cd7
Danil <danilbarabash@icloud.com>
2025-08-20 13:06:51 +0300

    Add activity_settings Updated files: strings.xml, AndroidManifest.xml

---
+++ app/src/main/java/com/example/englishwordsappn/SettingsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.englishwordsappn
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+
+
+    }
+}
